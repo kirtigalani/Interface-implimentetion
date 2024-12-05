6a440672200b6ab1a08312b133c94939c5332121
kirtigalani <kittugalani@gmail.com>
2024-12-05 21:30:12 +0530

    Add project files.

---
+++ Interface implimentetion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+ 
+    interface ICustomer
+    {
+        void InterfaceMethod();
+    }
+    interface ICustomer1
+    {
+        void InterfaceMethod();
+    }
+    public class Program : ICustomer , ICustomer1
+    {
+        public void InterfaceMethod()
+        {
+            Console.WriteLine("ICustomer Interface Method");
+        }
+        void ICustomer1.InterfaceMethod()
+        {
+            Console.WriteLine("ICustomer1 Interface Method");
+        }
+    public static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.InterfaceMethod();
+            ((ICustomer1)p).InterfaceMethod();
+    }
+}
+
